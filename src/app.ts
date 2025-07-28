5dd918d4e81fbc49fd49ff7aa84ee916f573745b
f3l1p3v4 <fvaldez018@gmail.com>
2025-07-27 23:31:27 -0400

    test

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ app.use(
       'http://localhost:5173',
       'http://127.0.0.1:5173',
       'https://musicoson.creathor.com.br',
+      'https://musicoson.teste.com.br',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
