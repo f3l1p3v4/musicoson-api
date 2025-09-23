bd5c014d0bc8840053bb93672f52ce46a86a3c5d
f3l1p3v4 <fvaldez018@gmail.com>
2025-09-23 16:50:43 -0400

    added dist to ignore

--- dist/application/schemas/UpdateTaskStatusSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateTaskStatusSchema = void 0;
-const zod_1 = require("zod");
-exports.updateTaskStatusSchema = zod_1.z.object({
-    status: zod_1.z.enum(['PENDING', 'COMPLETED']),
-});
-//# sourceMappingURL=UpdateTaskStatusSchema.js.map
