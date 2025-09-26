053a0098cb920f3f35b9872d3764858349131c52
f3l1p3v4 <fvaldez018@gmail.com>
2025-09-25 21:36:50 -0400

    changed scripts/deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,15 @@ set -e  # Encerra o script se qualquer comando falhar
 # Diretório do projeto
 cd /home/musicoson-api
 
-# Atualiza imagens Docker e reinicia os containers
+# Atualiza imagens Docker
 docker compose down || true  # Ignora erros se o container não existir
-docker compose build --no-cache  # Força rebuild para pegar alterações
+docker compose build --no-cache
+
+# Executa as migrações do banco de dados antes de subir a aplicação
+echo "Executando migrações do Prisma..."
+docker compose run --rm musicoson-api npx prisma migrate deploy
+
+# Inicia os containers em background
 docker compose up -d
 
 # Limpa imagens antigas (opcional)
